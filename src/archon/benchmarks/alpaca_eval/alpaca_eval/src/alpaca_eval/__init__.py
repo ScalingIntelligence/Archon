3ea67c1c9347fe1e153992f6ee635074b65d90c0
Shlok Natarajan <shlok.natarajan@gmail.com>
2024-10-20 21:23:18 -0700

    fix: fresh clone to fix submodules

--- src/archon/benchmarks/alpaca_eval/alpaca_eval/src/alpaca_eval/__init__.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from .main import *  # noqa
-
-__version__ = "0.6.5"
--- src/archon/benchmarks/alpaca_eval/alpaca_eval/src/alpaca_eval/metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from .helpers import SCORING_RULES
-from .winrate import *
-
-try:
-    from .glm_winrate import *
-except ImportError as e:
-    logging.warning(f"glm_winrate is not available. Error: {e}")
